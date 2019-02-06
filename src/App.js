9cbf1f9ddf0fe804b0096b3ee345bc0437765d22
madhusudhan518 <pvenkatamadhusudhanarao@gmail.com>
2019-02-06 14:54:58 +0530

    modified sentence

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={favi} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            In rebase branch
           </p>
           <a
             className="App-link"
